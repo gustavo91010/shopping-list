742b0dab94a0ebf8dc0c658f43dd519ba2d11873
gustavo91010 <gustavo910@gmail.com>
2024-11-02 16:11:27 -0300

    feat: completed implementation adapter view

--- app/src/main/java/com/ajudaqui/shopping_list/ui/activity/MainActivity.kt
+++ app/src/main/java/com/ajudaqui/shopping_list/ui/activity/MainActivity.kt
@@ -3,9 +3,12 @@ package com.ajudaqui.shopping_list.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ajudaqui.shopping_list.R
+import com.ajudaqui.shopping_list.modelo.Produto
 import com.ajudaqui.shopping_list.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
     override fun onCreate(savedInsaceState: Bundle?) {
@@ -14,7 +17,15 @@ class MainActivity : Activity() {
 // A classe R, é a classe que captura todos os idś que são gerados
         setContentView(R.layout.activity_main)
         var recycleView = findViewById<RecyclerView>(R.id.recycleView)
-        recycleView.adapter= ListaProdutosAdapter()
+        //recycleView.adapter= ListaProdutosAdapter()
+        recycleView.adapter = ListaProdutosAdapter(
+            context = this, produtos = listOf(
+                Produto(nome = "teste 1", descricao = "tesc desc 1", valor = BigDecimal("16.98")),
+                Produto(nome = "teste 2", descricao = "tesc desc 2", valor = BigDecimal("45.98"))
+            )
+        )
+        // essa configuraçãço de exibicao pode ser feita tanto via codigo aqui mesmo, quanto no layout
+        recycleView.layoutManager= LinearLayoutManager(this)
 
     }
 }
--- app/src/main/java/com/ajudaqui/shopping_list/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/com/ajudaqui/shopping_list/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -1,25 +1,53 @@
 package com.ajudaqui.shopping_list.ui.recyclerview.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.ajudaqui.shopping_list.R
 import com.ajudaqui.shopping_list.modelo.Produto
 
-class ListaProdutosAdapter (
-    private val produtos: List<Produto>
-): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+class ListaProdutosAdapter(
+    private val context: Context,
+    private val produtos: List<Produto>,
+) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
+
+
+    // essa classe s o adapter vai utilizar...
+    // eu recebo uma view, e devolvo uma viewHolder
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun vincular(produto: Produto) {
+            val nome = itemView.findViewById<TextView>(R.id.name)
+            nome.text = produto.nome
+            itemView.findViewById<TextView>(R.id.descricao).text = produto.descricao
+            itemView.findViewById<TextView>(R.id.valor).text = produto.valor.toPlainString()
+        }
     }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.produto_item, parent, false)
+        return ViewHolder(view)
+
+    }
+
 
     override fun getItemCount(): Int {
 
         return produtos.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // o metodo que vai indicar qual o item, a position e qual o rodem que esta com a view especifica
+        // ele vai pegar o prodto, vai mandar para o view holer, e lá vai ser feito a conversao
 
-        TODO("Not yet implemented")
+        val produto = produtos[position]
+        holder.vincular(produto)
+
     }
+
 
 }
